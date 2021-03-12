3afe97984b37b9b1da431bce2cc02bc1caea1453
Zach Chuba <zdc6@njit.edu>
2021-03-12 16:06:30 +0000

    Added front-end tests for major features

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("Checks that user list not active with login button", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const loginButton = screen.getByText('Join Room');
+  expect(loginButton).toBeInTheDocument();
+  const userListElement = screen.queryByText('Users Online');
+  expect(userListElement).not.toBeInTheDocument();
+});
+
+test("Ensure props state isLoggedIn changes and sets login invisible", () => {
+  render(<App />);
+  
+  const loginButton = screen.getByText('Join Room');
+  expect(loginButton).toBeInTheDocument();
+  fireEvent.click(loginButton);
+  expect(loginButton).not.toBeInTheDocument();
+});
+
+test("Test that the play again button is not present if the game is not over", () => {
+  render(<App />);
+  
+  const loginButton = screen.getByText('Join Room');
+  expect(loginButton).toBeInTheDocument();
+  fireEvent.click(loginButton);
+  
+  const playAgainButton = screen.queryByText('Play Again?');
+  expect(playAgainButton).not.toBeInTheDocument();
 });
